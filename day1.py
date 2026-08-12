d242c88f2047f7353ffbc03343428d124b2cce4a
sneha <snehareddyvem@gmail.com>
2026-08-12 09:26:42 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,71 @@
+##TASK-1
+name = "sneha"
+age = 21
+city = "warangal"
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+##task-2
+student_name = "sneha"
+student_age = 21
+student_marks = 85
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Student Marks:", student_marks)
+
+##task-3
+name = "sneha"#this store name
+age = 21#this store age
+print("Name:",name)
+print("Age:",age)
+
+##task-4
+'''
+the program displays messages like
+it  welcomes the user to python
+it also says python is easy to learn
+and also says user is learning python
+'''
+print("Welcome to Python!")
+print("Python is easy to learn")
+print("I am learning python")
+
+##task-5
+name = "sneha"
+age = 21
+marks = 85.5
+is_student = True
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+print("Is Student:", is_student)
+
+##task-6
+name,age,marks = "sneha",21,85.5
+print("Name:",name)
+print("Age:",age)
+print("Marks:",marks)
+
+##task-7
+a = 34
+print(a)
+a = 46
+print(a)
+
+##task-8
+a = 10
+b = 20
+print("before:", a,b)
+a,b = b,a
+print("after:", a,b)
+
+##task-9
+a = 5
+print(a)
+del a
+print(a)
+
+##task-10
+import keyword
+print(keyword.kwlist)
